6647fb9f25442696f7437df287a51ba24d48f248
vectrization <208457853+vectrization@users.noreply.github.com>
2026-03-02 19:58:34 +0800

    feat pages: added redirection from / to /introductions/welcome

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,5 +22,8 @@ export default defineConfig({
     },
     syntaxHighlight: "shiki",
     rehypePlugins: [rehypeAddClasses, rehypeSlug, [rehypeAutolinkHeadings, {behavior: 'wrap'}]]
+  },
+  redirects: {
+    '/': '/introduction/welcome'
   }
 });
